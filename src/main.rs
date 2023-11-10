464473227a9077b3c3aa28343aa8518f2a09086b
Owen Brady <owenbrady1994@gmail.com>
2023-11-10 10:03:00 +0000

    Simple TCP listener setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener =
+        TcpListener::bind("127.0.0.1:5050").expect("Failed to create TCP listener on address");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let peer_address = stream
+                    .peer_addr()
+                    .expect("Could not determine the peer address");
+                println!(
+                    "INFO: Connection successfully established with {}",
+                    peer_address
+                )
+            }
+            Err(err) => eprintln!("ERROR: Failed to establish connection: {}", err),
+        }
+    }
 }
