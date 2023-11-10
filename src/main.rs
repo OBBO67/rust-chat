ab0edb27878237f98916678c1b13440f330a5d0d
Owen Brady <owenbrady1994@gmail.com>
2023-11-10 12:08:44 +0000

    Add building blocks of client-server connection with channels

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,72 @@
-use std::net::TcpListener;
+use crate::Message::{ClientConnected, ClientDisconnected};
+use std::{
+    net::{TcpListener, TcpStream},
+    sync::mpsc::{self, Receiver, Sender},
+    thread,
+};
+
+struct Client {
+    address: String,
+}
+
+impl Client {
+    fn new(address: String) -> Self {
+        Client { address }
+    }
+}
+
+enum Message {
+    ClientConnected { client: Client },
+    ClientDisconnected,
+}
+
+fn server(msg_receiver: Receiver<Message>) {
+    loop {
+        let message = msg_receiver.recv().expect("Recevier channel hung up");
+
+        match message {
+            ClientConnected { client } => {
+                println!("INFO: Client connected from address {}", client.address);
+            }
+            ClientDisconnected => {
+                println!("INFO: Client disconnected");
+            }
+        }
+    }
+}
+
+fn client(stream: TcpStream, msg_sender: Sender<Message>) {
+    let client_address = match stream.peer_addr() {
+        Ok(address) => address,
+        Err(err) => {
+            eprintln!("ERROR: Could not get the client address: {}", err);
+            msg_sender
+                .send(ClientDisconnected)
+                .expect("Failed to send disconnect message");
+            return;
+        }
+    };
+
+    let client = Client::new(client_address.to_string());
+
+    msg_sender
+        .send(ClientConnected { client })
+        .expect("Failed to send connection message");
+}
 
 fn main() {
     let listener =
         TcpListener::bind("127.0.0.1:5050").expect("Failed to create TCP listener on address");
 
+    let (msg_sender, msg_receiver) = mpsc::channel::<Message>();
+
+    thread::spawn(|| server(msg_receiver));
+
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let peer_address = stream
-                    .peer_addr()
-                    .expect("Could not determine the peer address");
-                println!(
-                    "INFO: Connection successfully established with {}",
-                    peer_address
-                )
+                let msg_sender = msg_sender.clone();
+                thread::spawn(move || client(stream, msg_sender));
             }
             Err(err) => eprintln!("ERROR: Failed to establish connection: {}", err),
         }
